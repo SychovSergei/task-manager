02d3e6ddc9b1c3b6f4e986bb8c55fc459b264cd0
SychovSergei <sychov.sergei@gmail.com>
2023-11-07 15:27:46 +0200

    add sortArray function to utils.ts

--- client/src/app/utils/utils.ts
+++ client/src/app/utils/utils.ts
@@ -1,3 +1,5 @@
+import {ITaskItem} from "../interfaces/manage-task.interface";
+
 export class Utils {
 
   static generateId(length: number = 6): string {
@@ -10,5 +12,29 @@ export class Utils {
     }
     return randomIndex;
   }
+
+}
+
+export function sortArray<T>( array: T[], sortBy: string, sortOrder: 'asc' | 'desc' | '' ): T[] {
+  if (sortOrder === '') {
+    return array;
+  }
+
+  array.sort((a,b) => {
+    const valA: string = (a[sortBy as keyof T] as any).toString().toUpperCase();
+    const valB: string = (b[sortBy as keyof T] as any).toString().toUpperCase();
+
+    if (sortOrder === 'asc') {
+      if (valA < valB) return -1;
+      if (valA > valB) return 1;
+      return 0;
+    } else if (sortOrder === 'desc') {
+      if (valA > valB) return -1;
+      if (valA < valB) return 1;
+      return 0;
+    } else return 0;
+  })
+
+  return array;
 
 }

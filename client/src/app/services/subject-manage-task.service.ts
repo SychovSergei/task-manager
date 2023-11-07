8257638a96fbb587ac55259234ebb8db7db0977c
SychovSergei <sychov.sergei@gmail.com>
2023-11-07 20:21:29 +0200

    Add mock data to service

--- client/src/app/services/subject-manage-task.service.ts
+++ client/src/app/services/subject-manage-task.service.ts
@@ -8,8 +8,111 @@ import {TaskService} from "./http-manage-task.service";
 export class SubjectManageTaskService extends TaskService {
 
   mockTasksSubject: BehaviorSubject<ITaskItem[]> = new BehaviorSubject<ITaskItem[]>( [
-    {id: '1', completed: true, title: 'Title 1', description: 'Description 1', dateCreate: 1569033022000},
-    {id: '2', completed: false, title: 'Title 2', description: 'Description 2', dateCreate: 1569035022000},
+    {
+      id: '1',
+      title: 'Завершить проект Angular',
+      description: 'Закончить разработку проекта по управлению задачами на Angular.',
+      completed: false,
+      dateCreate: Date.now() - 86400000 // Вчера
+    },
+    {
+      id: '2',
+      title: 'Подготовить презентацию',
+      description: 'Подготовить презентацию для конференции.',
+      completed: false,
+      dateCreate: Date.now() - 172800000 // Позавчера
+    },
+    {
+      id: '3',
+      title: 'Сделать покупки',
+      description: 'Список продуктов для покупок в магазине.',
+      completed: true,
+      dateCreate: Date.now() - 259200000 // Три дня назад
+    },
+    {
+      id: '4',
+      title: 'Прочитать новую книгу',
+      description: 'Новая книга по разработке на Angular.',
+      completed: false,
+      dateCreate: Date.now() - 345600000 // Четыре дня назад
+    },
+    {
+      id: '5',
+      title: 'Подготовить ужин',
+      description: 'Рецепт для ужина сегодня вечером.',
+      completed: true,
+      dateCreate: Date.now() - 432000000 // Пять дней назад
+    },
+    {
+      id: '6',
+      title: 'Подготовить отчет',
+      description: 'Подготовить месячный отчет для руководства.',
+      completed: false,
+      dateCreate: Date.now() - 518400000 // Шесть дней назад
+    },
+    {
+      id: '7',
+      title: 'Занятия спортом',
+      description: 'Пойти на тренировку в спортзал.',
+      completed: true,
+      dateCreate: Date.now() - 604800000 // Одна неделя назад
+    },
+    {
+      id: '8',
+      title: 'Подготовить подарок',
+      description: 'Подготовить подарок для друга на день рождения.',
+      completed: false,
+      dateCreate: Date.now() - 691200000 // Восемь дней назад
+    },
+    {
+      id: '9',
+      title: 'Изучить новый фреймворк',
+      description: 'Изучить новый JavaScript фреймворк.',
+      completed: false,
+      dateCreate: Date.now() - 777600000 // Девять дней назад
+    },
+    {
+      id: '10',
+      title: 'Путешествие в выходные',
+      description: 'План путешествия на ближайшие выходные.',
+      completed: true,
+      dateCreate: Date.now() - 864000000 // Десять дней назад
+    },
+    {
+      id: '11',
+      title: 'Подготовить отчет',
+      description: 'Отчет о прошедшей неделе работы.',
+      completed: false,
+      dateCreate: Date.now() - 518400000 // Шесть дней назад
+    },
+    {
+      id: '12',
+      title: 'Планирование отпуска',
+      description: 'Подготовка к отпуску на следующей неделе.',
+      completed: false,
+      dateCreate: Date.now() - 604800000 // Одна неделя назад
+    },
+    {
+      id: '13',
+      title: 'Убраться в гараже',
+      description: 'Очистить гараж от старых вещей.',
+      completed: true,
+      dateCreate: Date.now() - 691200000 // Две недели назад
+    },
+    {
+      id: '14',
+      title: 'Сходить в спортзал',
+      description: 'Тренировка в спортзале вечером.',
+      completed: false,
+      dateCreate: Date.now() - 777600000 // Три недели назад
+    },
+    {
+      id: '15',
+      title: 'Создать блог по программированию',
+      description: 'Начать писать статьи о программировании.',
+      completed: false,
+      dateCreate: Date.now() - 864000000 // Месяц назад
+    }
   ]);
 
   constructor() {

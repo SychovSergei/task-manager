c0cb274e292639f36691c2d7a9f56b0f9886492e
SychovSergei <sychov.sergei@gmail.com>
2024-02-25 21:05:02 +0200

    change server index.js
    change quotes to double quotes

--- server/index.js
+++ server/index.js
@@ -1,22 +1,17 @@
-// const urlDB = 'mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true';
 require('dotenv').config();
-const express = require('express');
-
-const { createServer } = require('http');
+const express = require("express");
+const { createServer } = require("http");
 const cors = require("cors");
-// const router = require("./routes/index");
-// const errorHandler = require("./middleware/ErrorHandleMiddleware");
 
-const PORT = process.env.PORT || 9200;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api", router);
-app.get("/", (req, res, next) => {
-  res.send('"Server SP works..."');
-  next();
+
+app.get("/", (req, res) => {
+  res.send("Server SP works...");
 });
 
 const start = () => {
@@ -25,10 +20,10 @@ const start = () => {
     /** Create Server */
     const server = createServer(app);
 
-    server.listen(PORT, () => console.log(`Server started on port`, PORT));
+    server.listen(PORT, () => console.log("Server started on port", PORT));
 
   } catch (e) {
-    console.log('Start() ERROR', e);
+    console.log("Start() ERROR", e);
   }
 }
 

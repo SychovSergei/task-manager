8ff7cf1c85a404c09830889ad6a0d36d1fd48ca6
SychovSergei <sychov.sergei@gmail.com>
2024-03-12 22:13:59 +0200

    Delete firebase functions folder and firebase hosting

--- server/functions/src/routes/testRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const testController = require("../controllers/testController");
-
-router.get("/data", testController.testData);
-
-module.exports = router;

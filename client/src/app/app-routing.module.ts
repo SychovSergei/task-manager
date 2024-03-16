1ca7d1ea762c835815461597b7a5e7741328feab
SychovSergei <sychov.sergei@gmail.com>
2024-03-17 00:51:38 +0200

    Add some routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './common/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: '/auth/login',
+  // },
+  {
+    path: "auth",
+    component: AuthComponent,
+    children: [
+      {
+        path: "login",
+        component: LoginComponent,
+        data: {
+          title: "Login"
+        }
+      },
+      {
+        path: "register",
+        component: RegisterComponent,
+        data: {
+          title: "Registration"
+        }
+      },
+      {
+        path: "confirm-email/:token",
+        component: RegisterComponent,
+        data: {
+          title: "Confirm Email"
+        }
+      },
+    ]
+  },{
+    path: "dashboard",
+    component: DashboardComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

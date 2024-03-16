ec0993e7573f223958a5e23fd48c9b9c28edb62e
SychovSergei <sychov.sergei@gmail.com>
2024-03-17 00:51:14 +0200

    Add layout in app component
    - add side navigation
    - add side menu
    - add footer
    - add topbar
    - add response behaviour for side menu

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BreakpointService, EBreakpoints } from './services/breakpoint.service';
+import { Config, MenuItem } from './common/menu/menu.interfaces';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  isMobile: boolean = false;
+  isTablet: boolean = false;
+  isDesktop: boolean = false;
+  isLargeDesktop: boolean = false;
+
+  constructor(public breakpointService: BreakpointService) {
+    this.breakpointService.getCurrScreenSize()
+      .subscribe((screenSize) => {
+        this.isMobile = screenSize === EBreakpoints.XSmall;
+        this.isTablet = screenSize === EBreakpoints.Small;
+        this.isDesktop = screenSize === EBreakpoints.Medium;
+        this.isLargeDesktop = screenSize === (EBreakpoints.Large || EBreakpoints.XLarge);
+      });
+  }
+
+  options: Config = {
+    multi: false
+  };
+
+  menus: MenuItem[] = [
+    {
+      title: 'Dashboard',
+      iconSource: {
+        type: "mat-icon",
+        matIconData: {
+          iconCode: "dashboard"
+        }
+      },
+      url: "/dashboard",
+      active: false,
+    },
+    {
+      title: 'Auth',
+      iconSource: {
+        type: "fontawesome",
+        matIconData: {
+          iconClass: 'fa fa-mobile',
+        }
+      },
+      active: false,
+      submenu: [
+        { title: "Login", url: "/auth/login" },
+        { title: "Register", url: "/auth/register" },
+      ]
+    },
+  ];
+
+
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,25 +2,47 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {TaskListModule} from "./task-list/task-list.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from "@angular/material/card";
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { LoginModule } from './auth/login/login.module';
+import { ToolbarModule } from './common/toolbar/toolbar.module';
+import { FooterModule } from './common/footer/footer.module';
+import { MenuModule } from './common/menu/menu.module';
+import { DashboardModule } from './common/dashboard/dashboard.module';
+import { TaskListModule } from "./task-list/task-list.module";
+
+import { BreakpointService } from './services/breakpoint.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
 
-    MatCardModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
 
     TaskListModule,
+
+    AuthModule,
+    LoginModule,
+    ToolbarModule,
+    FooterModule,
+    MenuModule,
+    DashboardModule,
   ],
-  providers: [],
+  providers: [
+    BreakpointService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

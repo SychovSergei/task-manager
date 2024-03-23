c2f4a1cf5e0d52e94218676e076f9f5581871739
SychovSergei <sychov.sergei@gmail.com>
2024-03-23 17:46:04 +0200

    [fix] Replace lines

--- server/src/bin/app.ts
+++ server/src/bin/app.ts
@@ -4,8 +4,9 @@ import cors from "cors";
 import router from "../routes";
 
 const app: Express = express();
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+
 app.use("/api", router);
 export default app;

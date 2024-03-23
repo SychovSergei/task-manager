eabf53f699aed2210b64a59c4e4038498e84b87a
SychovSergei <sychov.sergei@gmail.com>
2024-03-23 16:39:15 +0200

    Create LoginComponent

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "../auth.service";
+import { IUserLoginDto, IUserRegistrationDto } from "../register/user.model";
+import { catchError, of, tap } from "rxjs";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+  isSubmit: boolean = false;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+        email: new FormControl("sychov.sergei@gmail.com", [
+          Validators.required, Validators.email
+        ]),
+        password: new FormControl("123456", [
+          Validators.required, Validators.minLength(6)
+        ]),
+        remember: new FormControl(false),
+      }
+    )
+  }
+
+  login() {
+    const user: IUserLoginDto = {
+      email: this.loginForm.get('email')!.value,
+      password: this.loginForm.get('password')!.value,
+    };
+
+    this.authService.login(user)
+      .pipe(
+        tap((res) => {
+          console.log("res===", res); }),
+        catchError((error) => {
+          console.log('Error caught:', error);
+          // TODO handle the error here
+          return of('Fallback Value');
+        })
+      )
+      .subscribe({
+        next: (result) => {
+          console.log("next result ===", result);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+  }
 }
--- client/src/app/auth/login/login.module.ts
+++ client/src/app/auth/login/login.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterLink } from "@angular/router";
 
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+
+import { LoginComponent } from './login.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    RouterLink,
+
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
   ],
   declarations: [
     LoginComponent,

9cffe9d42a9abf4277b6b0633fa935be8368a500
SychovSergei <sychov.sergei@gmail.com>
2024-03-23 17:37:17 +0200

    [feat] Add cookie-parser to project and add into app.ts

--- server/src/bin/app.ts
+++ server/src/bin/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import router from "../routes";
 
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api", router);
 export default app;

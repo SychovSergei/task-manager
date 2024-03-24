fd7e43f9b87c41be10ead873a2996f08eb867bbb
SychovSergei <sychov.sergei@gmail.com>
2024-03-24 10:26:38 +0200

    [conf] Add fileReplacements field for prod and dev modes
    replace 'src/environment.ts' to 'src/environments/environment.(prod/dev).ts'

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { IUserLoginDto, IUserRegistrationDto } from "./register/user.model";
+import { ServerUrlService } from "../services/server-url.service";
 
 @Injectable()
 export class AuthService {
 
   private registrationUrl: string = "/api/auth/registration";
   private loginUrl: string = "/api/auth/login";
+  private registrationUrl: string = this.serverUrl.getServerUrl() + "/auth/registration";
+  private loginUrl: string = this.serverUrl.getServerUrl() + "/auth/login";
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private serverUrl: ServerUrlService) {
   }
 
   registration(userData: IUserRegistrationDto) {
